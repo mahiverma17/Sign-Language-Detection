6b62c49c3d970cd6058c45cefd2d2262a11bdf42
mahiverma17 <107355258+mahiverma17@users.noreply.github.com>
2024-08-04 18:11:13 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,69 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("/Users/MahiVerma/Desktop/Model/keras_model.h5" , "/Users/MahiVerma/Desktop/Model/labels.txt")
+offset = 20
+imgSize = 300
+counter = 0
+
+labels = ["Hello","I Love You","Yes"]
+
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+    imgOutput = img.copy()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+
+        y1 = max(0, y - offset)
+        y2 = min(img.shape[0], y + h + offset)
+        x1 = max(0, x - offset)
+        x2 = min(img.shape[1], x + w + offset)
+
+        imgCrop = img[y1:y2, x1:x2]
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize-wCal)/2)
+            imgWhite[:, wGap: wCal + wGap] = imgResize
+            prediction , index = classifier.getPrediction(imgWhite, draw= False)
+            print(prediction, index)
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap: hCal + hGap, :] = imgResize
+            prediction , index = classifier.getPrediction(imgWhite, draw= False)
+
+       
+        cv2.rectangle(imgOutput,(x-offset,y-offset-70),(x -offset+400, y - offset+60-50),(0,255,0),cv2.FILLED)  
+
+        cv2.putText(imgOutput,labels[index],(x,y-30),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,0),2) 
+        cv2.rectangle(imgOutput,(x-offset,y-offset),(x + w + offset, y+h + offset),(0,255,0),4)   
+
+        cv2.imshow('ImageCrop', imgCrop)
+        cv2.imshow('ImageWhite', imgWhite)
+
+    cv2.imshow('Image', imgOutput)
+    cv2.waitKey(1)
+    
